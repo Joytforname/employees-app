96ffd5d3bff4b60c533317d0ef58da5c7a72b169
Joyt <bigboom1122@gmail.com>
2023-12-13 13:13:56 +0300

    localstorage auth

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,6 +8,7 @@ import Login from './pages/Login';
 import './index.css';
 import { ConfigProvider, theme } from 'antd';
 import { store } from './app/store';
+import Auth from './features/auth/auth';
 
 const router = createBrowserRouter([
 	{
@@ -27,8 +28,10 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<ConfigProvider theme={{algorithm: theme.darkAlgorithm}}>
-				<RouterProvider router={router} />
+			<ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
+				<Auth>
+					<RouterProvider router={router} />
+				</Auth>
 			</ConfigProvider>
 		</Provider>
 	</React.StrictMode>
