6e161ef0f8020b762d5726fdf1b69824501029ce
Joyt <bigboom1122@gmail.com>
2023-12-17 23:46:06 +0300

    port 2

--- client/src/app/services/api.ts
+++ client/src/app/services/api.ts
@@ -3,7 +3,7 @@ import { RootState } from '../store';
 
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: 'http://localhost:10000/api',
+	baseUrl: 'http://localhost:8000/api',
 	prepareHeaders: (headers, { getState }) => {
 		const token = (getState() as RootState).auth.user?.token || 
 		localStorage.getItem('token')
