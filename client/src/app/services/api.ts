6124b090f79b7ce96d488a052a3f7def4ec29d06
Joyt <bigboom1122@gmail.com>
2023-12-18 00:43:34 +0300

    deploy edit

--- client/src/app/services/api.ts
+++ client/src/app/services/api.ts
@@ -3,13 +3,14 @@ import { RootState } from '../store';
 
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: 'http://localhost:8000/api',
+	baseUrl: 'https://employee-crm.onrender.com/api',
 	prepareHeaders: (headers, { getState }) => {
-		const token = (getState() as RootState).auth.user?.token || 
-		localStorage.getItem('token')
+		const token =
+			(getState() as RootState).auth.user?.token ||
+			localStorage.getItem('token');
 
 		if (token || token !== null) {
-			headers.set('authorization', `Bearer ${token}`)
+			headers.set('authorization', `Bearer ${token}`);
 		}
 		return headers;
 	},
--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -13,7 +13,7 @@ import ErrorMessage from '../../components/error-message';
 const Login = () => {
 	const navigate = useNavigate();
 	// const [loginUser, loginUserResult] = useLoginMutation();
-		const [loginUser] = useLoginMutation();
+	const [loginUser] = useLoginMutation();
 	const [error, setError] = useState('');
 	const login = async (data: UserData) => {
 		try {
